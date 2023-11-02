8588c194c31f2da0e4fd60291d118f0542bea3b3
BernhardClemm <bernhard.clemm@mail.de>
2023-11-02 11:32:03 +0100

    Projections accuracy plots

--- code/ep-projections-acc.R
+++ code/ep-projections-acc.R
@@ -9,14 +9,17 @@
 library(tidyverse)
 path <- paste0(dirname(dirname(rstudioapi::getSourceEditorContext()$path)), "/")
 
-# DATA =========================================================================
+# COUNTRIES ====================================================================
 
-ep_proj_00 <- read.csv(paste0(path, "data/ft-ep-projections.csv"))
-ep_res_00 <- read.csv(paste0(path, "data/ft-ep-results.csv"))
+## data ####
 
-# RECODING =====================================================================
+ep_proj_00 <- read.csv(paste0(path, "data/ft-ep-countries-projections.csv"))
+ep_res_00 <- read.csv(paste0(path, "data/ft-ep-countries-results.csv"))
 
-# projections ####
+## recoding ####
+
+### projections
+
 ep_proj_01 <- ep_proj_00 %>%
   mutate(across(epp:re, ~ as.numeric(gsub(" \\(.*\\)", "", .))))
 
@@ -26,12 +29,12 @@ ep_proj_uk <- ep_proj_01 %>%
   filter(name == "United Kingdom") %>%
   select(-c(new, id, re)) %>%
   group_by(name) %>%
-  summarize(across(epp:enf, ~ sum(., na.rm = TRUE)))
+  dplyr::summarize(across(epp:enf, ~ sum(., na.rm = TRUE)))
 ep_proj_be <- ep_proj_01 %>%
   filter(name == "Belgium") %>%
   select(-c(efdd, gue, ni, id, re)) %>%
   group_by(name) %>%
-  summarize(across(epp:enf, ~ sum(., na.rm = TRUE)))
+  dplyr::summarize(across(epp:enf, ~ sum(., na.rm = TRUE)))
 
 ep_proj_02 <- ep_proj_01 %>%
   filter(!name %in% c("United Kingdom", "Belgium")) %>%
@@ -43,7 +46,8 @@ ep_proj_l <- ep_proj_02 %>%
   pivot_longer(epp:re, names_to = "group", values_to = "seats_proj") %>%
   filter(!is.na(seats_proj))
   
-# results ####
+### results 
+
 ep_res_01 <- ep_res_00 %>%
   mutate(across(epp:re, ~ as.numeric(gsub(" \\(.*\\)", "", .)))) %>%
   select(-subname)
@@ -52,7 +56,7 @@ ep_res_l <- ep_res_01 %>%
   pivot_longer(epp:re, names_to = "group", values_to = "seats_res") %>%
   filter(!is.na(seats_res)) 
 
-# harmonize group names and ignore certain groups ####
+## harmonize group names and ignore certain groups ####
 ## RE = ALDE and ID = ENF
 
 ep_res_l <- ep_res_l %>% 
@@ -75,9 +79,7 @@ ep_res_l <- ep_res_l %>%
 ep_proj_l <- ep_proj_l %>% 
   filter(!group %in% c("ni", "new", "efdd"))
 
-# RESULTS ======================================================================
-
-# join projections/results and compute mean absolute error ####
+## join projections ####
 
 ep <- ep_proj_l %>%
   left_join(., ep_res_l, by = c("name", "group")) %>%
@@ -85,43 +87,90 @@ ep <- ep_proj_l %>%
     e = seats_res - seats_proj,
     ae = abs(seats_res - seats_proj))
 
-# mean absolute error on group-country level ####
+## Results ####
 
-mae <- mean(ep$ae, na.rm = T)
+### Mean absolute error on group-country level
+
+mean(ep$ae, na.rm = T)
+
+### plot
 
 e_plot <- ep %>% ggplot(aes(y = e, x = group, fill = group)) +
   geom_bar(stat = "identity") +
   scale_fill_manual(
     values = c("#ffe033", "#217a9d", "#58aafc", "#321079", 
                "#8deb9d", "#991c00", "#f95f5f")) +
-  facet_wrap(~name) +
-  theme_light()
+  scale_y_continuous(name = "Projection error (in seats)") +
+  facet_wrap(~name, nrow = 4) +
+  theme_light() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = "bottom")
 
-ggsave(e_plot)
+ggsave(file = paste0(path, "output/ep-country-accuracy.png"),
+       height = 8, width = 9)
 
-# mean absolute error on group level ####
+# TOTALS =======================================================================
 
-## Quick and dirty: get this manually
-ep_total <- data.frame(
-  group = c("alde/re", "ecr", "enf/id", "epp", "greens", "gue", "sd"),
-  seats_proj = c(97, 57, 55, 171, 53, 43, 149),
-  seats_res = c(108, 62, 73, 182, 74, 41, 154),
-  name = "All countries")
+## data ####
 
-ep_total <- ep_total %>%
+ep_totals_proj_00 <- read.csv(paste0(path, "data/ft-ep-totals-projections.csv"))
+ep_totals_res_00 <- read.csv(paste0(path, "data/ft-ep-totals-results.csv"))
+
+## recoding ####
+ep_totals_proj_01 <- ep_totals_proj_00 %>%
+  rename("seats_proj" = n_seats) %>%
+  mutate(group_name = case_when(
+    group_name %in% c("ALDE", "RE")  ~ "ALDE/RE",
+    group_name %in% c("ENF", "ID")  ~ "ENF/ID",
+    TRUE ~ as.character(group_name)))
+
+ep_totals_res_01 <- ep_totals_res_00 %>%
+  rename("seats_res" = n_seats) %>%
+  mutate(group_name = case_when(
+    group_name %in% c("ALDE", "RE")  ~ "ALDE/RE",
+    group_name %in% c("ENF", "ID")  ~ "ENF/ID",
+    group_name == "Greens/EFA"  ~ "Greens EFA",
+    group_name == "GUE/NGL"  ~ "GUE NGL",
+    TRUE ~ as.character(group_name)))
+
+## join projections/results ####
+
+ep_totals <- ep_totals_proj_01 %>%
+  left_join(., ep_totals_res_01, by = c("group_name")) %>% 
+  # Ignore Non-inscrits, newcomers and efdd, as unclear whether fair comparison
+  filter(!group_name %in% c("NI", "New", "EFDD")) %>%
   mutate(
     e = seats_res - seats_proj,
-    ae = abs(seats_res - seats_proj))
+    ae = abs(seats_res - seats_proj)) %>%
+  mutate(name = "Totals")
 
-mae_total <- mean(ep_total$ae, na.rm = T)
+## Results ####
 
-ep_total_plot <- ep_total %>% ggplot(aes(y = e, x = group, fill = group)) +
+### Mean absolute error on group level
+
+mean(ep_totals$ae, na.rm = T)
+
+### Plot
+
+ep_totals_plot <- ep_totals %>% 
+  ggplot(aes(y = e, x = group_name, fill = group_name)) +
   geom_bar(stat = "identity") +
   scale_fill_manual(
     values = c("#ffe033", "#217a9d", "#58aafc", "#321079", 
                "#8deb9d", "#991c00", "#f95f5f")) +
+  scale_y_continuous(name = "Projection error (in seats)") +
   facet_wrap(~name) +
-  theme_light()
+  theme_light() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = "bottom")
+
+ggsave(file = paste0(path, "output/ep-totals-accuracy.png"),
+       height = 5, width = 5)
+
 
 
 
