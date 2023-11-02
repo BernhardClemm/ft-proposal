8d4234b19963015ce6baeff89384efcc368a0e98
BernhardClemm <bernhard.clemm@mail.de>
2023-11-02 12:13:52 +0100

    Update ep-projections-acc.R

--- code/ep-projections-acc.R
+++ code/ep-projections-acc.R
@@ -9,6 +9,66 @@
 library(tidyverse)
 path <- paste0(dirname(dirname(rstudioapi::getSourceEditorContext()$path)), "/")
 
+# TOTALS =======================================================================
+
+## data ####
+
+ep_totals_proj_00 <- read.csv(paste0(path, "data/ft-ep-totals-projections.csv"))
+ep_totals_res_00 <- read.csv(paste0(path, "data/ft-ep-totals-results.csv"))
+
+## recoding ####
+ep_totals_proj_01 <- ep_totals_proj_00 %>%
+  rename("seats_proj" = n_seats) %>%
+  mutate(group_name = case_when(
+    group_name %in% c("ALDE", "RE")  ~ "ALDE/RE",
+    group_name %in% c("ENF", "ID")  ~ "ENF/ID",
+    TRUE ~ as.character(group_name)))
+
+ep_totals_res_01 <- ep_totals_res_00 %>%
+  rename("seats_res" = n_seats) %>%
+  mutate(group_name = case_when(
+    group_name %in% c("ALDE", "RE")  ~ "ALDE/RE",
+    group_name %in% c("ENF", "ID")  ~ "ENF/ID",
+    group_name == "Greens/EFA"  ~ "Greens EFA",
+    group_name == "GUE/NGL"  ~ "GUE NGL",
+    TRUE ~ as.character(group_name)))
+
+## join projections/results ####
+
+ep_totals <- ep_totals_proj_01 %>%
+  left_join(., ep_totals_res_01, by = c("group_name")) %>% 
+  # Ignore Non-inscrits, newcomers and efdd, as unclear whether fair comparison
+  filter(!group_name %in% c("NI", "New", "EFDD")) %>%
+  mutate(
+    e = seats_res - seats_proj,
+    ae = abs(seats_res - seats_proj)) %>%
+  mutate(name = "Totals")
+
+## Results ####
+
+### Mean absolute error on group level
+
+mean(ep_totals$ae, na.rm = T)
+
+### Plot
+
+ep_totals_plot <- ep_totals %>% 
+  ggplot(aes(y = e, x = group_name, fill = group_name)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(
+    values = c("#ffe033", "#217a9d", "#58aafc", "#321079", 
+               "#8deb9d", "#991c00", "#f95f5f")) +
+  scale_y_continuous(name = "Projection error (in seats)") +
+  facet_wrap(~name) +
+  theme_light() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = "bottom")
+
+ggsave(file = paste0(path, "output/ep-totals-accuracy.png"),
+       height = 5, width = 6.5)
+
 # COUNTRIES ====================================================================
 
 ## data ####
@@ -111,65 +171,7 @@ e_plot <- ep %>% ggplot(aes(y = e, x = group, fill = group)) +
 ggsave(file = paste0(path, "output/ep-country-accuracy.png"),
        height = 8, width = 9)
 
-# TOTALS =======================================================================
 
-## data ####
-
-ep_totals_proj_00 <- read.csv(paste0(path, "data/ft-ep-totals-projections.csv"))
-ep_totals_res_00 <- read.csv(paste0(path, "data/ft-ep-totals-results.csv"))
-
-## recoding ####
-ep_totals_proj_01 <- ep_totals_proj_00 %>%
-  rename("seats_proj" = n_seats) %>%
-  mutate(group_name = case_when(
-    group_name %in% c("ALDE", "RE")  ~ "ALDE/RE",
-    group_name %in% c("ENF", "ID")  ~ "ENF/ID",
-    TRUE ~ as.character(group_name)))
-
-ep_totals_res_01 <- ep_totals_res_00 %>%
-  rename("seats_res" = n_seats) %>%
-  mutate(group_name = case_when(
-    group_name %in% c("ALDE", "RE")  ~ "ALDE/RE",
-    group_name %in% c("ENF", "ID")  ~ "ENF/ID",
-    group_name == "Greens/EFA"  ~ "Greens EFA",
-    group_name == "GUE/NGL"  ~ "GUE NGL",
-    TRUE ~ as.character(group_name)))
-
-## join projections/results ####
-
-ep_totals <- ep_totals_proj_01 %>%
-  left_join(., ep_totals_res_01, by = c("group_name")) %>% 
-  # Ignore Non-inscrits, newcomers and efdd, as unclear whether fair comparison
-  filter(!group_name %in% c("NI", "New", "EFDD")) %>%
-  mutate(
-    e = seats_res - seats_proj,
-    ae = abs(seats_res - seats_proj)) %>%
-  mutate(name = "Totals")
-
-## Results ####
-
-### Mean absolute error on group level
-
-mean(ep_totals$ae, na.rm = T)
-
-### Plot
-
-ep_totals_plot <- ep_totals %>% 
-  ggplot(aes(y = e, x = group_name, fill = group_name)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(
-    values = c("#ffe033", "#217a9d", "#58aafc", "#321079", 
-               "#8deb9d", "#991c00", "#f95f5f")) +
-  scale_y_continuous(name = "Projection error (in seats)") +
-  facet_wrap(~name) +
-  theme_light() +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        legend.position = "bottom")
-
-ggsave(file = paste0(path, "output/ep-totals-accuracy.png"),
-       height = 5, width = 5)
 
 
 
